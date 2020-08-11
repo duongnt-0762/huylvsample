6f7963a15188a5c1ddd29f1490a45a7f4a8eae57
lehuy95 <huylevp95@gmail.com>
2020-08-11 15:17:12 +0700

    --amend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	before_save :ba_duong_vlog
+	before_save :downcase_email
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 },
 					  format: { with: VALID_EMAIL_REGEX },
@@ -7,7 +7,9 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
-	def ba_duong_vlog
+	private
+
+	def downcase_email
 		self.email = email.downcase
 	end
 end
