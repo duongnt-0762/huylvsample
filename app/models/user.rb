549a51c17a236d6d58145bd1e1721339491a6b52
lehuy95 <huylevp95@gmail.com>
2020-08-12 21:25:58 +0700

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,15 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
 
+	class << self
+	# Returns the hash digest of the given string.
+		def digest(string)
+			cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+													  BCrypt::Engine.cost
+			BCrypt::Password.create(string, cost: cost)
+		end
+	end	
+
 	private
 
 	def downcase_email
